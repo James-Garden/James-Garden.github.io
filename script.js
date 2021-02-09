018c1c034694a95de12f580b08c70c0b775a72c4
James <jamesegarden@outlook.com>
2021-02-09 10:26:22 +0000

    the void begins

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-function changeText() {
-  let x = document.getElementById('testP').innerHTML;
-  if (x=='test1') {
-    document.getElementById('testP').innerHTML = 'test2';
-  } else {
-    document.getElementById('testP').innerHTML = 'test1';
-  }
-}
-
-var testPress = document.getElementById('testBtn');
-testPress.addEventListener('click',changeText);
+var searchHidden = true; //This defines the initial state of the search bar
+$(document).ready(function(){ //This function triggers when the page is fully loaded
+  //This is the function for opening the search bar
+  //$(".search-bar").hide(); //This closes the search bar when the page loads
+  $(".button-search").click(function(){
+    if (searchHidden) {
+      searchHidden = false;
+      $(".search-bar").slideDown("slow");
+    } else {
+      searchHidden = true;
+      $(".search-bar").slideUp("slow");
+    }
+  });
+  //This is the function for opening the side menu
+  $(".button-menu").click(function(){
+    document.getElementById("sidenav").style.width = "15vw";
+  });
+  //This is the function for closing the side menu
+  $(".button-close-side-menu").click(function(){
+    document.getElementById("sidenav").style.width = "0";
+  });
+});

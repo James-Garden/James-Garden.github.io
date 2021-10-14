c73adfbc809b5b61c2dd0921d291c1abb3f3856d
James Garden <jamesegarden@outlook.com>
2021-10-14 01:14:12 +0100

    changed image paths to S3

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         <?php
           $result = $mysqli->query("SELECT media.cover_image,media.name,media.description FROM media INNER JOIN book ON media.media_id = book.book_id ORDER BY RAND() LIMIT 2;");
           foreach ($result as $row) {
-            echo "<table><td><img src=\"cover_images/" . $row['cover_image'] . "\" width=\"259\" height=\"384\"></td>";
+            echo "<table><td><img src=\"https://mymedialist.s3.eu-west-2.amazonaws.com/cover_images/" . $row['cover_image'] . "\" width=\"259\" height=\"384\"></td>";
             echo "<td><h2>" . $row['name'] ."</h2>";
             echo "<p>".$row['description']."</p></td></table>";
           }
@@ -19,7 +19,7 @@
         <?php
           $result = $mysqli->query("SELECT media.cover_image,media.name,media.description FROM media INNER JOIN film ON media.media_id = film.film_id ORDER BY RAND() LIMIT 2;");
           foreach ($result as $row) {
-            echo "<table><td><img src=\"cover_images/" . $row['cover_image'] . "\" width=\"259\" height=\"384\"></td>";
+            echo "<table><td><img src=\"https://mymedialist.s3.eu-west-2.amazonaws.com/cover_images/" . $row['cover_image'] . "\" width=\"259\" height=\"384\"></td>";
             echo "<td><h2>" . $row['name'] ."</h2>";
             echo "<p>".$row['description']."</p></td></table>";
           }
@@ -29,7 +29,7 @@
         <?php
           $result = $mysqli->query("SELECT media.cover_image,media.name,media.description FROM media INNER JOIN tv ON media.media_id = tv.tv_id ORDER BY RAND() LIMIT 2;");
           foreach ($result as $row) {
-            echo "<table><td><img src=\"cover_images/" . $row['cover_image'] . "\" width=\"259\" height=\"384\"></td>";
+            echo "<table><td><img src=\"https://mymedialist.s3.eu-west-2.amazonaws.com/cover_images/" . $row['cover_image'] . "\" width=\"259\" height=\"384\"></td>";
             echo "<td><h2>" . $row['name'] ."</h2>";
             echo "<p>".$row['description']."</p></td></table>";
           }
